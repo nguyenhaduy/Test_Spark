3307f863c7dc5785c26a5772eb6807434eedcf6e
Duy Nguyen <nguyenhaduy@gmail.com>
2017-06-12 01:57:27 +0530

    Parameterized Frequent Direction

--- Frequent_Direction/frequent_direction.py
+++ Frequent_Direction/frequent_direction.py
@@ -1,26 +1,44 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
-from numpy import zeros, max, sqrt, isnan, isinf, dot, diag, count_nonzero
+from numpy import zeros, max, sqrt, isnan, isinf, dot, diag, count_nonzero, where
 from numpy.linalg import svd, linalg, LinAlgError, norm
 from scipy.linalg import svd as scipy_svd
-#from scipy.sparse.linalg import svds as scipy_svds
 
 
-# In[2]:
-
-
-class FrequentDirections():
-
-    def __init__(self , rows, columns):
+class FrequentDirections(object):
+    
+    def __init__(self , rows, columns, op='fd'):
+        """
+        """
         self.class_name = 'FrequentDirections'
+        self.op = op
         self.columns = columns
         self.rows = rows
         self.sketchMatrix = zeros((self.rows, self.columns)) 
+        self.S = np.zeros(self.columns)
+        self.U = []
+        self.Vt = []
+        self.step = 1
         self.nextZeroRow = 0
+        self.emptyRows = self.rows
+        if self.op == 'fd':
+            print("Matrix Sketching Using Frequent Direction")
+            self.reduceRank = self.__FDOperate__
+        elif self.op == 'ssd':
+            print("Matrix Sketching Using Space Saving Direction")
+            self.op = 2
+            self.reduceRank = self.__SSDOperate__
+        elif self.op == 'cfd':
+            print("Matrix Sketching Using Compensative Freqent Direction")
+            self.reduceRank = self.__CFDperate__
+        elif self.op == 'isvd':
+            print("Matrix Sketching Using iSVD")
+            self.reduceRank = self.__iSVDOperate__
+        elif type(self.op) != str and self.op > 0 and self.op < 1:
+            print("Matrix Sketching Using Parameterized Frequent Direction")
+            self.reduceRank = self.__PFDOperate__
+            self.DELTA = 0
+        else:
+            print("Type of Reduce Rank algorithm is not correct")
+            raise ValueError
     
     # Add new vector to the sketch matrix
     def add(self,vector):     
@@ -28,24 +46,118 @@ class FrequentDirections():
             return
         
         # If the approximate matrix is full, call the operate method to free half of the columns
-        if self.nextZeroRow >= self.rows:
-            self.__operate__()
-
+#         if self.nextZeroRow >= self.rows:
+        if self.emptyRows <= 0:
+            try:
+                [self.U,self.S,self.Vt] = svd(self.sketchMatrix , full_matrices=True)
+            except LinAlgError as err:
+                [self.U,self.S,self.Vt] = scipy_svd(self.sketchMatrix , full_matrices = True)
+            self.reduceRank()
+#             Push the new vector to the next zero row
+#             self.sketchMatrix[where(self.S == 0)[0][0],:] = vector
         # Push the new vector to the next zero row and increase the next zero row index
-        self.sketchMatrix[self.nextZeroRow,:] = vector 
+#         else:    
+        self.sketchMatrix[self.nextZeroRow,:] = vector
         self.nextZeroRow += 1
+        self.emptyRows -= 1
 
 
-    # Shrink the approximate matrix
-    def __operate__(self):
-        # Calculating SVD
-        [U,s,Vt] = svd(self.sketchMatrix , full_matrices=False)
-        #Shrink the sketch matrix
-        self.sketchMatrix[:len(s),:] = dot(diag(s), Vt[:len(s),:])
-        self.sketchMatrix[int(len(s)/2):,:] = 0
-        self.nextZeroRow = int(len(s)/2)
+    # Shrink the approximate matrix using Frequent Direction
+    def __FDOperate__(self):
+        if len(self.S) >= self.rows:
+            # Calculating matrix s
+            delta = sqrt(self.S[:]**2 - self.S[len(self.S)-1]**2)
+            self.S = delta
+            self.S[len(self.S)-1] = 0
+            print (self.S)
+            #Shrink the sketch matrix
+            self.sketchMatrix[:,:] = dot(diag(self.S), self.Vt[:self.rows,:])
+            self.nextZeroRow = (len(self.S)-1)
+            self.emptyRows += 1
+        else:
+#             s = zeros((self.rows,self.columns))
+#             s[:len(self.S), :len(self.S)] = np.diag(self.S)
+            self.sketchMatrix[:len(self.S),:] = dot(diag(self.S), self.Vt[:len(self.S),:])
+#             self.sketchMatrix[len(self.S):,:] = 0
+            self.nextZeroRow = len(self.S)-1
+            self.emptyRows += 1
+            
         
-    # Return the sketch matrix
-    def get(self):
-        return self.sketchMatrix[:self.rows,:]
+    # Shrink the approximate matrix using iterative SVD
+    def __iSVDOperate__(self):
+        # Calculating matrix s
+        self.S[len(self.S)-1] = 0
+        print (self.S)
+        #Shrink the sketch matrix
+        self.sketchMatrix[:len(self.S),:] = dot(diag(self.S), self.Vt[:len(self.S),:])
+        self.nextZeroRow = (len(self.S)-1)
+        self.emptyRows += 1
 
+
+   	# Shrink the approximate matrix using Parameterized FD
+    def __PFDOperate__(self):
+        #Shrink the sketch matrix
+        if len(self.S) >= self.rows:
+            # Calculating matrix s
+#             delta = sqrt(self.S[:round(len(self.S)*(1-self.op))]**2 - self.S[-1]**2)
+            delta = self.S[-1]**2
+            print (self.S)
+            self.S[round(len(self.S)*(1-self.op)):] = sqrt(self.S[round(len(self.S)*(1-self.op)):]**2 - self.S[-1]**2)
+            print (self.S)
+            #Shrink the sketch matrix
+            self.sketchMatrix[:,:] = dot(diag(self.S), self.Vt[:self.rows,:])
+            self.nextZeroRow = len(self.S) - 1
+            self.emptyRows += 1
+        else:
+            self.sketchMatrix[:len(self.S),:] = dot(diag(self.S), Vt[:len(self.S),:])
+            self.sketchMatrix[int(len(self.S)/2):,:] = 0
+            self.nextZeroRow = int(len(self.S)/2)
+            self.emptyRows += 1
+
+    # Shrink the approximate matrix using Space Saving Direction
+    def __SSDOperate__(self):
+        # Calculating matrix s
+        self.S[-1] = sqrt(self.S[-1]**2 + self.S[-2]**2)
+        self.S[len(self.S)-2] = 0
+        print (self.S)
+        #Shrink the sketch matrix
+        self.sketchMatrix[:len(self.S),:] = dot(diag(self.S), self.Vt[:len(self.S),:])
+        self.nextZeroRow = len(self.S)-2
+        self.emptyRows += 1
+        
+        
+    # Shrink the approximate matrix using Compensative Direction
+    def __CFDperate__(self):
+        # Calculating SVD
+        try:
+            [U,self.S,Vt] = svd(self.sketchMatrix, full_matrices=False)
+        except LinAlgError as err:
+            [U,self.S,Vt] = scipy_svd(self.sketchMatrix, full_matrices = False)
+        # Calculating matrix s
+        self.DELTA += self.S[-1]**2
+        self.S = sqrt(self.S[:len(self.S)]**2 + self.DELTA)
+        delta = sqrt(self.S[:]**2 - self.S[len(self.S)-1]**2)
+        self.S = delta
+        self.S[len(self.S)-1] = 0
+        print (self.S)
+        #Shrink the sketch matrix
+        self.sketchMatrix[:len(self.S),:] = dot(diag(self.S), self.Vt[:len(self.S),:])
+        self.nextZeroRow = len(self.S) - 1
+        self.emptyRows += 1
+
+
+    # Return the sketch matrix
+    def getSketchMatrix(self):
+        return self.sketchMatrix[:self.rows,:]
+    
+    # Return the S matrix
+    def getS(self):
+        return self.S
+    
+    # Return the U matrix
+    def getU(self):
+        return self.U
+    
+    # Return the Vt matrix
+    def getVt(self):
+        return self.Vt
